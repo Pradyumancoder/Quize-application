41c803de9a4e9297f8276e0785b8e79bfe779e66
pradyuman shukla <shuklapradyuman786@gmail.com>
2022-12-23 14:57:15 +0530

    quizeapp

--- code-fellas-master/quize_app/context/Auth.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext, useState } from "react";
-
-
-export const AuthContext=createContext();
-
-export const AuthProvider=({children})=>{
-    const [credData,setCredData]=useState({token:"",isAuth:false,role:""})
-    return <AuthContext.Provider value={{credData,setCredData}}>{children}</AuthContext.Provider>
-}
--- code-fellas-master/quize_app/db.connect.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connect = async () => {
-return  mongoose.connect(process.env.MONGODB_URI);
-};
